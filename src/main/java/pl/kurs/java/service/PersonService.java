518514de5908da41a94e6b7564527fecb01b4039
MateyszSiedleckiJava <siedla96@gmail.com>
2021-04-29 19:31:16 +0200

    adding data to postgres

--- src/main/java/pl/kurs/java/service/PersonService.java
+++ src/main/java/pl/kurs/java/service/PersonService.java
@@ -3,10 +3,18 @@ package pl.kurs.java.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import pl.kurs.java.model.Employee;
 import pl.kurs.java.model.Person;
+import pl.kurs.java.model.Retirement;
+import pl.kurs.java.model.Student;
 import pl.kurs.java.model.query.FindPersonQuery;
+import pl.kurs.java.repository.EmployeeRepository;
 import pl.kurs.java.repository.PersonRepository;
+import pl.kurs.java.repository.RetirementRepository;
+import pl.kurs.java.repository.StudentRepository;
 
+import javax.annotation.PostConstruct;
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -33,35 +41,35 @@ public class PersonService {
     }
 
 
-//    private final StudentRepository studentRepository;
-//    private final EmployeeRepository employeeRepository;
-//    private final RetirementRepository retirementRepository;
-//    @PostConstruct
-//    public void init() {
-//        Student student1 = new Student("Mateusz", "Siedlecki", "87063043979", "programming", "IT", 7, 4.5);
-//        Student student2 = new Student("Arek", "Marek", "72110541412", "programming", "IT", 3, 3.5);
-//
-//        studentRepository.saveAndFlush(student1);
-//        studentRepository.saveAndFlush(student2);
-//
-//        Employee employee1 = new Employee("Urszula", "Kowal", "07323032725", "Kasjer",
-//                "Zabka", 3000, LocalDate.of(2021, 03, 21));
-//        Employee employee2 = new Employee("Dorota", "Kowal", "06291279444", "Kasjer",
-//                "Zabka", 3200, LocalDate.of(2021, 04, 21));
-//
-//        employeeRepository.saveAndFlush(employee1);
-//        employeeRepository.saveAndFlush(employee2);
-//
-//        Retirement retirement1 = new Retirement("Agata", "Nowak", "60081051663", 10, 2000);
-//        Retirement retirement2 = new Retirement("Magda", "Kowalska", "50073157664", 10, 2000);
-//        Retirement retirement3 = new Retirement("Dorota", "Filipowicz", "89051881727", 10, 2000);
-//        Retirement retirement4 = new Retirement("Iwona", "Stec", "47041788641", 10, 2000);
-//
-//        retirementRepository.saveAndFlush(retirement1);
-//        retirementRepository.saveAndFlush(retirement2);
-//        retirementRepository.saveAndFlush(retirement3);
-//        retirementRepository.saveAndFlush(retirement4);
-//
-//    }
+    private final StudentRepository studentRepository;
+    private final EmployeeRepository employeeRepository;
+    private final RetirementRepository retirementRepository;
+    @PostConstruct
+    public void init() {
+        Student student1 = new Student("Mateusz", "Siedlecki", "87063043979", "programming", "IT", 7, 4.5);
+        Student student2 = new Student("Arek", "Marek", "72110541412", "programming", "IT", 3, 3.5);
+
+        studentRepository.saveAndFlush(student1);
+        studentRepository.saveAndFlush(student2);
+
+        Employee employee1 = new Employee("Urszula", "Kowal", "07323032725", "Kasjer",
+                "Zabka", 3000, LocalDate.of(2021, 03, 21));
+        Employee employee2 = new Employee("Dorota", "Kowal", "06291279444", "Kasjer",
+                "Zabka", 3200, LocalDate.of(2021, 04, 21));
+
+        employeeRepository.saveAndFlush(employee1);
+        employeeRepository.saveAndFlush(employee2);
+
+        Retirement retirement1 = new Retirement("Agata", "Nowak", "60081051663", 10, 2000);
+        Retirement retirement2 = new Retirement("Magda", "Kowalska", "50073157664", 10, 2000);
+        Retirement retirement3 = new Retirement("Dorota", "Filipowicz", "89051881727", 10, 2000);
+        Retirement retirement4 = new Retirement("Iwona", "Stec", "47041788641", 10, 2000);
+
+        retirementRepository.saveAndFlush(retirement1);
+        retirementRepository.saveAndFlush(retirement2);
+        retirementRepository.saveAndFlush(retirement3);
+        retirementRepository.saveAndFlush(retirement4);
+
+    }
 }
 
